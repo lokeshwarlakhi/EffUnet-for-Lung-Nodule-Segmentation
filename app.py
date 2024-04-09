a99612cd9286bcb528dc34c2a8d540b570a17eda
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2024-04-09 23:16:49 +0530

    app developed

--- app.py
+++ app.py
@@ -1,72 +1,66 @@
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+import os
+import cv2
+import random
 import numpy as np
-from tensorflow.keras.layers import Layer
-# from keras.models import load_model
-# import segmentation_models as sm
-# from segmentation_models import Unet
-# from segmentation_models import get_preprocessing
-# from tensorflow.keras.utils import plot_model
-# sm.set_framework('tf.keras')
-# sm.framework()
-# BACKBONE = 'efficientnetb2'
+import pandas as pd 
+from io import BytesIO
+import tensorflow as tf
+import pydicom as dicom
+from tensorflow import keras
+from keras.models import load_model 
+from keras import backend as K
+import matplotlib.pyplot as plt
+os.environ["SM_FRAMEWORK"] = "tf.keras"
+import segmentation_models as sm
+from segmentation_models import Unet,get_preprocessing
+from keras.utils import plot_model
+from keras.callbacks import EarlyStopping ,ReduceLROnPlateau ,ModelCheckpoint
 
-# preprocess_input = get_preprocessing(BACKBONE)
-# model = Unet(BACKBONE, encoder_weights='imagenet')
+st.title("Efficient UNet Model in Action")
 
-# model = model.load_weights('seg_model.h5')
+# def load_eff_unet_model():
+sm.set_framework('tf.keras')
+sm.framework()
+BACKBONE = 'efficientnetb2'
+preprocess_input = get_preprocessing(BACKBONE)
+model = Unet(BACKBONE, encoder_weights='imagenet')
+model.compile(optimizer='adam', loss=sm.losses.DiceLoss(), metrics=[tf.keras.metrics.Recall(),
+                                    tf.keras.metrics.Precision(),
+                                    tf.keras.metrics.AUC(),
+                                    sm.metrics.iou_score,
+                                    sm.metrics.FScore(threshold=0.5)])
+# return model
 
-
-class FixedDropout(Layer):
-    def __init__(self, rate, **kwargs):
-        super(FixedDropout, self).__init__(**kwargs)
-        self.rate = rate
-
-    def call(self, inputs, training=None):
-        if training:
-            return tf.nn.dropout(inputs, rate=self.rate)
-        return inputs
-
-
-model = tf.keras.models.load_model('seg_model.h5')
-
+# model = load_eff_unet_model()
+model.load_weights('/home/lokeswarlakhineni/codespace/lung Nodule segmentation using Res Unet/archived_weights/model_1.weights.h5')
 
 
 
-def preprocess_image(img_path, target_size=(256, 256)):
-    img = image.load_img(img_path, target_size=target_size)
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize pixel values to [0, 1]
-    return img_array
 
-def segment_image(img_array):
-    segmentation = model.predict(img_array)
-    return segmentation[0, :, :, 0]
+def predict(image):
+    img = cv2.resize(image,(256, 256)) 
+    img = np.array(img)
+    # img = np.stack((img,) * 3, axis=-1)
+    img = img/img.max()
+    img = img.astype(np.float32)
+    img = img.reshape(1,img.shape[0],img.shape[1],img.shape[2])
+    prediction = model.predict(img)
+    return prediction
 
-def main():
-    st.title("CT Scan Nodule Segmentation")
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    uploaded_file = st.file_uploader("Choose a CT scan image", type=["jpg", "jpeg", "png"])
-
-    if uploaded_file is not None:
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-
-        if st.button("Start Segmentation"):
-            pass
-            # Preprocess the uploaded image
-            img_array = preprocess_image(uploaded_file)
-            
-            # Perform segmentation
-            segmentation_result = segment_image(img_array)
-
-            # Display original and segmented images
-            st.image([img_array[0], segmentation_result], 
-                     caption=["Original Image", "Segmentation Result"], 
-                     use_column_width=True, format='png')
-
-            # Provide a description based on your analysis
-            st.write("Nodule analysis description goes here.")
-
-if __name__ == "__main__":
-    main()
+if uploaded_file is not None:
+    file_bytes = uploaded_file.read()
+    nparr = np.frombuffer(file_bytes, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    col1, col2 = st.columns(2)
+    # with col1:
+    st.image(image, caption='Image', use_column_width=True)
+        # st.image(input_image, caption='Input Image', use_column_width=True)
+    if st.button('Start Prediction'):
+        prediction = predict(image)
+        # prediction=(prediction > 0.5).astype(np.uint8)
+        # with col1:
+        # st.image(image, caption='Image', use_column_width=True)
+        st.image(prediction, caption='Image', use_column_width=True)
--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,16 @@
 import numpy as np
 import cv2
+img_width = 256
+img_height = 256
 
-
-def preprocess(img):
+def preprocess_img(img):
+    img = cv2.resize(img, (img_width, img_height)) 
     img = np.array(img)
     img = np.stack((img,) * 3, axis=-1)
+    img = img/img.max()
+    img = img.astype(np.float32)
+    return img
+
+    
+    
+
